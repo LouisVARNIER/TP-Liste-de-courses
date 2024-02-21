c1b3bb291cd7ced8f9407757f7f962098be80d20
louis <louis.varnier@hotmail.com>
2024-02-21 12:56:38 +0100

    first commit

---
+++ liste.js
@@ -0,0 +1,30 @@
+const panier = [];
+
+const form = document.getElementById("form");
+const objet = document.getElementById("objet");
+const liste = document.getElementById("liste");
+const button = document.getElementById("button");
+
+form.onsubmit = (event) => {
+    event.preventDefault();
+
+switch (button.value) {
+    case 'pomme':
+        panier.push("pomme");
+        break;
+    case 'banane':
+        panier.push("banane");
+        break;
+    case 'ananas':
+            panier.push("ananas");
+            break;
+    case 'mangue':
+        panier.push("mangue");
+        break;            
+    default:
+        break;
+}
+
+    liste.innerText = panier;
+
+};
